bd6adf564f4082fba6d58be00b502a25e47fcd46
ljyh223 <3439426154@qq.com>
2025-12-30 19:42:46 +0800

    店员管理

--- lib/l10n/generated/translations.g.dart
+++ lib/l10n/generated/translations.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 3
 /// Strings: 882 (294 per locale)
 ///
-/// Built on 2025-12-30 at 11:37 UTC
+/// Built on 2025-12-30 at 11:41 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
